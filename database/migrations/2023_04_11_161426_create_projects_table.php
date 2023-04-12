acfddcdf39e6cc14c534d6801837084b9fe5e935
SofiaCurreri <sofiacurreri@gmail.com>
2023-04-12 16:50:40 +0200

    seeded database

--- database/migrations/2023_04_11_161426_create_projects_table.php
+++ database/migrations/2023_04_11_161426_create_projects_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->string('title',50);
+            $table->string('title',100);
             $table->string('slug',100)->unique();
             $table->string('image')->nullable();
             $table->text('text');
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +15,15 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker) //dependency injection
     {
-        //
+        for($i = 0; $i < 40; $i++) {
+            $project = new Project;
+            $project->title = $faker->catchPhrase();
+            $project->slug = Str::of($project->title)->slug('-');
+            $project->image= $faker->imageUrl(640, 480, 'animals', true);
+            $project->text = $faker->paragraph(15);
+            $project->save();
+        }
     }
 }
