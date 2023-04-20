8df1b5e7f0daf555b4f0c2ea83f9876190cf6e1e
SofiaCurreri <sofiacurreri@gmail.com>
2023-04-20 15:02:57 +0200

    soft delete

--- database/migrations/2023_04_11_161426_create_projects_table.php
+++ database/migrations/2023_04_11_161426_create_projects_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('slug',100)->unique();
             $table->text('image')->nullable();
             $table->text('text');
+            $table->softDeletes();
             $table->timestamps();
 
         });
